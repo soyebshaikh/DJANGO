097b8e024302190b489ab43d582bddd9af1eb961
soyebshaikh <81480317+soyebshaikh@users.noreply.github.com>
2022-10-16 22:56:01 -0700

    Add files via upload

---
+++ products/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . models import Product
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+ list_display = ('name', 'price')
+admin.site.register(Product, ProductAdmin)
